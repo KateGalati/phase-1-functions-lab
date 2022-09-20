d8f1da758ccfb997c106cfb1252ed05b983ddc11
Kate Galati <kategalati@Kates-MacBook-Air.local>
2022-09-20 14:43:17 -0700

    commit tree f13f526911110f5f6a86e8a2934f1d545745e4c9

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(pickupLocal) {
+    if (pickupLocal > 42) {
+        return pickupLocal - 42;
+    } 
+      else {
+        return 42 - pickupLocal;
+      }
+    }
+
+function distanceFromHqInFeet(currentBlock) {
+    //distanceFromHqInBlocks(blocksInFeet);
+
+    let blocksInFeet = distanceFromHqInBlocks(currentBlock);
+    return blocksInFeet * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if ( start >= destination) {
+        return (start - destination) * 264;
+    } else {
+        return (destination - start) * 264;
+    }
+}
+
+function calculatesFarePrice(startBlock, endBlock) {
+    let distance = distanceTravelledInFeet(startBlock, endBlock)
+
+    if (distance <= 400) {
+        return 0;
+    }
+    else if (distance <= 2000) {
+        return (distance - 400) * .02;
+    }
+    else if (distance <= 2500) {
+        return 25;
+    }
+    else {
+        return "cannot travel that far";
+    }
+}
